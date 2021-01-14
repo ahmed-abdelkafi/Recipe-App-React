ef6ac1a78c904d0b2577f1d5eeb457fbed6898a6
ahmed <ahmed.sastec@gmail.com>
2021-01-14 02:47:16 +0100

    Recipe Page Structure

---
+++ src/components/Recipe.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Recipe extends Component {
+    render() {
+        return (
+            <div>
+                Hello from recipe
+            </div>
+        );
+    }
+}
+
+export default Recipe;
--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -1,10 +1,32 @@
 import React, {Component} from 'react';
+import RecipeList from "../components/RecipeList";
+import Search from "../components/Search";
+import {recipeData} from "../data/tempList";
 
 class Recipes extends Component {
+    constructor(props){
+        super(props);
+    }
+    state = {
+        recipes :recipeData,
+        search:''
+    }
+    handleChange = (e) => {
+        this.setState({
+            search :e.target.value
+        })
+    }
+    handleSubmit = (e) => {
+        e.preventDefault();
+
+    }
+
     render() {
         return (
             <div>
-                Hello From Recipes
+                <Search search={this.state.search} handleChange={this.handleChange} handleSubmit={this.handleSubmit} />
+                <RecipeList recipe={this.state.recipes}/>
+
             </div>
         );
     }
