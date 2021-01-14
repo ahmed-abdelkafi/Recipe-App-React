987b8ff74941812924945a5877dc0e1814801c6d
ahmed <ahmed.sastec@gmail.com>
2021-01-14 03:19:52 +0100

    Search Component

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,9 +2,39 @@ import React, {Component} from 'react';
 
 class Search extends Component {
     render() {
+        const { handleChange , handleSubmit , search} = this.props;
         return (
-            <div>
-                Hello from Search
+            <div className="container">
+                <div className="row">
+                    <div className="col-10 mx-auto col-md-8 mt-5 text-center">
+                        <h1 className="text-slanted text-capitalize">
+                            search recipes with <strong style={{color:"#f15025"}}>Recipes2Cook</strong>
+                        </h1>
+
+                <form >
+                        <label htmlFor="search" className="text-capitalize">
+                            types recipes separated by comma
+                        </label>
+                    <div className="input-group">
+                        <input
+                            type = "text"
+                            name="search"
+                            placeholder={"onion,tomato,chicken"}
+                            value={search}
+                            onChange={handleChange}
+                            className="form-control"/>
+                        <div className="input-group-append">
+                            <button type="submit"
+                                    onClick={handleSubmit}
+                                    className="input-group-text bg-primary text-white"
+                            >
+                                <i className="fas fa-search"/>
+                            </button>
+                        </div>
+                    </div>
+                </form>
+                    </div>
+                </div>
             </div>
         );
     }
